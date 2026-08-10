e6b0d3af448cf75f8be214ecf93e8c309c4f1f6a
Ivan <posket.klimovich@gmail.com>
2026-08-10 13:47:42 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-DB_USER = "myuser"          # ваш пользователь
-DB_PASSWORD = "mypass"      # ваш пароль
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "botanica"
+import os
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
